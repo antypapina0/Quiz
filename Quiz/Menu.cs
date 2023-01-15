59d29357fd2776d9c2f6e83a6b413990e0068c3d
mloda <abc@email.com>
2023-01-16 00:34:30 +0100

    kolor

--- Quiz/Menu.cs
+++ Quiz/Menu.cs
@@ -42,7 +42,9 @@ namespace Quiz
 
         private void button6_Click(object sender, EventArgs e)
         {
-
+            panel1.BackColor = Color.SteelBlue;
+            label1.BackColor = Color.SteelBlue;
+            label2.BackColor = Color.SteelBlue;
 
         }
 
@@ -54,12 +56,16 @@ namespace Quiz
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            panel1.BackColor = Color.CornflowerBlue;
+            label1.BackColor = Color.CornflowerBlue;
+            label2.BackColor = Color.CornflowerBlue;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            panel1.BackColor = Color.White;
+            label1.BackColor = Color.White;
+            label2.BackColor = Color.White;
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
